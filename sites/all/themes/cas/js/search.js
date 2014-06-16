f5f0758ad2daeb34bc345b2201886e606efca00e
Chris Morris <cmorris@ccistudios.com>
2014-06-16 17:00:54 -0400

    Accessibility Fixes

--- sites/all/themes/cas/js/search.js
+++ sites/all/themes/cas/js/search.js
@@ -1,9 +1,11 @@
 (function($) {
 	$(window).load(function(){
-		$("#block-search-form").click(function(){
+		$("#block-search-form").prepend("<a href='#' class='search-btn' title='Search'>Search</a>");
+
+		$("#block-search-form a.search-btn").click(function(){
 			if ($("#block-search-form form").width() == 0 || $("#block-search-form form").css("display") == "none")
 			{
-				var width = getWidth($(this));
+				var width = getWidth($(this).parent());
 				$("#block-search-form form").css({
 					"display":"block",
 					"width":"0"
@@ -13,15 +15,18 @@
 				});
 				$("#edit-search-block-form--2").focus();
 			}
+
+			return false;
 		})
-		.find("#edit-search-block-form--2").blur(function(){
+		.parent().find("#edit-search-block-form--2").blur(function(){
 			$("#block-search-form form").animate({
 				"width":"toggle"
 			});
 		});
 
 
-		$("#footer-search").click(function(){
+		$("#footer-search").prepend("<a href='#' class='search-btn' title='Search'>Search</a>");
+		$("#footer-search a.search-btn").click(function(){
 			if ($("#footer-search form").width() == 0 || $("#footer-search form").css("display") == "none")
 			{
 				var width = 196;
@@ -34,8 +39,10 @@
 				});
 				$("#edit-search-block-form--4").focus();
 			}
+
+			return false;
 		})
-		.find("#edit-search-block-form--4").blur(function(){
+		.parent().find("#edit-search-block-form--4").blur(function(){
 			$("#footer-search form").animate({
 				"width":"toggle"
 			});
