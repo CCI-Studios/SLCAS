354402ae9b8939f41fef83b5c5f72c2c20a35c79
Chris Morris <cmorris@ccistudios.com>
2014-06-17 15:56:09 -0400

    Added drush configuration for live environment

--- sites/all/drush/aliases.drushrc.php
+++ sites/all/drush/aliases.drushrc.php
@@ -10,3 +10,13 @@ $aliases['dev'] = array(
 	),
 	'ssh-options'=>'-p 37241'
 );
+
+$aliases['live'] = array(
+	'uri'=> 'slcas.on.ca',
+	'root' => '/home/slcas/subdomains/live/public_html',
+	'remote-host'=> 'host.lambtonshield.com',
+	'remote-user'=> 'slcas',
+	'path-aliases'=> array(
+		'%files'=> 'sites/default/files',
+	)
+);
